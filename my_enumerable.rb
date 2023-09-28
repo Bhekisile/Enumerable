e0a33a1b0e57e0770ad98545a2a08b7eb376a148
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-09-28 16:36:11 +0200

    Create enumerable module with all method

---
+++ my_enumerable.rb
@@ -0,0 +1,17 @@
+module MyEnumerable
+
+  def all?(&block)
+    each do |item|
+      return false unless block.call(item)
+    end
+    true
+  end
+
+  def any?(&block)
+    each do |item|
+      return true if block.call(item)
+    end
+    false
+  end
+
+end
